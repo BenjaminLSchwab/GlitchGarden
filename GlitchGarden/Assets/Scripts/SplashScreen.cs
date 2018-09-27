609693d96000c35ff747dc0a0302339533bc4b7f
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-27 16:07:58 -0700

    splash screen, start screen

---
+++ GlitchGarden/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+    [SerializeField] float timeUntilNextLevel = 5;
+    float timer;
+    LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = GetComponent<LevelManager>();
+        timer = timeUntilNextLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer();
+	}
+
+    void Timer()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            levelManager.LoadLevel("Start");
+        }
+    }
+}
