f1a85ad8a9ddc75987539d55e6827755a2bbef16
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-04 16:24:23 -0700

    working music manager, screen aspect adjusts correctly

---
+++ GlitchGarden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] LevelMusicChangeArray;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        Debug.Log(level);
+        if (!LevelMusicChangeArray[level]) return;
+        audioSource.clip = LevelMusicChangeArray[level];
+        audioSource.Play();
+    }
+}
